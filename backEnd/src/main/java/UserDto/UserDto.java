79b3abfaf0eb2f95c5e1c0aa5e5c8ec734a96632
HebertAlv <107577409+HebertAlv@users.noreply.github.com>
2025-04-07 20:07:01 -0500

    Updated UserDTO

--- backEnd/src/main/java/UserDto/UserDto.java
+++ backEnd/src/main/java/UserDto/UserDto.java
@@ -2,7 +2,7 @@ package UserDto;
 
 
 public class UserDto {
-    private String id;
+    private int id;
 
     private String firstName;
 
@@ -14,5 +14,64 @@ public class UserDto {
 
     private String role;
 
-    private String positions;
+    private String[] positions;
+
+    public UserDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String[] getPositions() {
+        return positions;
+    }
+
+    public void setPositions(String[] positions) {
+        this.positions = positions;
+    }
 }
